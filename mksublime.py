3cc1439413d33f7a8516b7d46f1861a2881b3b5a
Ben Davis <bencallumdavis@gmail.com>
2014-03-16 12:18:58 +0000

    Added support for a SUBLIME_PROJECTS_DIR environment variable.

--- mksublime.py
+++ mksublime.py
@@ -8,11 +8,12 @@ def parse_args():
     """ Parse and return the arguments
 
     Returns:
-        args
+        args: the args namespace
 
     """
     parser = argparse.ArgumentParser(description="Creates a sublime project.")
-    parser.add_argument('project_directory', help='The directory containing project data.')
+    parser.add_argument('project_data_directory',
+                        help='The directory containing project data.')
     return parser.parse_args()
 
 
@@ -23,16 +24,25 @@ def main(args):
         args: the arguments passed to the script
 
     """
-    project_directory = os.path.abspath(args.project_directory)
-    project_name = getattr(args, 'name', None) or os.path.basename(project_directory)
-    project_file = os.path.join(project_directory, project_name + '.sublime-project')
+    # If SUBLIME_PROJECTS_DIR is set, then it takes precedence
+    sublime_projects_dir = os.path.normpath(os.getenv('SUBLIME_PROJECTS_DIR'))
+    project_data_directory = os.path.abspath(args.project_data_directory)
+    project_storage_directory = sublime_projects_dir or project_data_directory
 
+    # Get the name of the project either from the name arg or
+    # the project data dir
+    project_name = (getattr(args, 'name', None)
+                    or os.path.basename(project_data_directory))
+    project_file = os.path.join(project_storage_directory,
+                                project_name + '.sublime-project')
+
+    # The project settings, eventually output as json.
     data = {
         "folders":
         [
             {
                 "follow_symlinks": True,
-                "path": project_directory
+                "path": project_data_directory
             }
         ]
     }
@@ -45,8 +55,8 @@ def main(args):
         data['settings'] = {
             "python_intepreter": interpreter
         }
-    if not os.path.exists(project_directory):
-        os.makedirs(project_directory)
+    if not os.path.exists(project_storage_directory):
+        os.makedirs(project_storage_directory)
 
     # Write the data to the project file
     with open(project_file, 'w') as f:
